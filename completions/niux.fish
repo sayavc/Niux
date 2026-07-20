3ac2051b239ffdeaaeb1b3d8d48e9bd6900c717d
sayavc <sayme.git@email.com>
2026-07-20 19:48:38 +0300

    fix completions

--- completions/niux.fish
+++ completions/niux.fish
@@ -1,20 +1,25 @@
 function _niux_packages
-    niux -l
+    niux -l --raw
 end
 
 function _niux_home_packages
-    niux -Hl
+    niux -Hl --raw
 end
 
 function _niux_system_packages
-    niux -Sl
+    niux -Sl --raw
 end
 
 function _niux_search
     niux --search (commandline -ct)
 end
 
+function _niux_has_install
+    set -l cmd (commandline)
+    string match -q -r -- '(-Hi|-Si|-Hia|-Sia)' "$cmd"
+end
+
+complete -c niux -n "__fish_seen_subcommand_from -l" -f -a "(_niux_packages)"
 complete -c niux -n "__fish_seen_subcommand_from -Hl -Hr -Hra" -f -a "(_niux_home_packages)"
 complete -c niux -n "__fish_seen_subcommand_from -Sl -Sr -Sra" -f -a "(_niux_system_packages)"
-complete -c niux -n "__fish_seen_subcommand_from -l" -f -a "(_niux_packages)"
-complete -c niux -n "__fish_seen_subcommand_from -Hi -Si -Hia -Sia" -f -a "(_niux_search)"
+complete -c niux -n "_niux_has_install" -f -a "(_niux_search)"
